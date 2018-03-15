679d901d6973f4869dfd209701b174caa6baa034
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-03-16 00:02:07 +0100

    added done and delete buttons

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 $(function () {
     $('#add').click(function() {
         if ($('#item').val() !== "") {
-            $('#list').append('<li>' + $('#item').val() +  '</li>'); 
+            $('#list').append('<li style="border-bottom:1px solid #8faebc;"><span>' + $('#item').val() +  
+            '</span><button class="item-button btn btn-xs btn-danger single-remove">Delete</button> <button class="btn btn-xs btn-primary done">Done</button></li>'); 
 			$('#item').val('');
 		}
     });
-
 });
+
+$('#list').on('click', '.single-remove', function() {		
+    $(this).parent().hide(400, function(){
+        $(this).remove();
+    });		
+});
+$('#list').on('click','.done', function(){
+    $(this).parent().css('background-color', 'green');  
+});
+
